90aae04cdad11a99068f5941cdf25a78577e4145
VHarisop <the.machine.headed@gmail.com>
2016-02-16 13:22:20 +0200

    Fixed type warnings + compare() in Pair class

--- src/main/java/gr/demokritos/iit/jinsect/structs/Pair.java
+++ src/main/java/gr/demokritos/iit/jinsect/structs/Pair.java
@@ -14,17 +14,21 @@ import java.io.ObjectStreamException;
 import java.io.Serializable;
 import java.util.Objects;
 
-/** Represents a pair of elements of any type (as a templated class).
+/** 
+ * Represents a pair of elements of any type (as a templated class).
  *
  * @author ggianna
  */
 public class Pair<ObjTypeFirst, ObjTypeSecond> implements Serializable {
+	static final long serialVersionUID = 1L;
+
     protected ObjTypeFirst first;
     protected ObjTypeSecond second;
     
-    /** Creates a new instance of Pair, given two objects. 
-     *@param oFirst The first object.
-     *@param oSecond The second object.
+    /** 
+	 * Creates a new instance of Pair, given two objects. 
+     * @param oFirst The first object.
+     * @param oSecond The second object.
      */
     public Pair(ObjTypeFirst oFirst, ObjTypeSecond oSecond) {
         first = oFirst;
@@ -43,21 +47,29 @@ public class Pair<ObjTypeFirst, ObjTypeSecond> implements Serializable {
 
     @Override
     public boolean equals(Object obj) {
-        return (obj instanceof Pair) && (first.equals(((Pair)obj).getFirst()) &&
-                second.equals(((Pair)obj).getSecond()));
+		if (null == obj)
+			return false;
+		if (!(obj instanceof Pair))
+			return false;
+
+		Pair<?, ?> otherP = (Pair<?, ?>) obj;
+		return (first.equals(otherP.getFirst()) && 
+				second.equals(otherP.getSecond()));
     }
 
 
 
-    /** Returns the first object of the pair. 
-     *@return The first object. 
+    /** 
+	 * Returns the first object of the pair. 
+     * @return The first object. 
      */
     public ObjTypeFirst getFirst() {
         return first;
     }
 
-    /** Returns the second object of the pair. 
-     *@return The second object. 
+    /** 
+	 * Returns the second object of the pair. 
+     * @return The second object. 
      */
     public ObjTypeSecond getSecond() {
         return second;
@@ -69,10 +81,11 @@ public class Pair<ObjTypeFirst, ObjTypeSecond> implements Serializable {
         out.writeObject(second);
     }
 
+	@SuppressWarnings("unchecked")
     private void readObject(ObjectInputStream in)
          throws IOException, ClassNotFoundException {
-         first = (ObjTypeFirst)in.readObject();
-         second = (ObjTypeSecond)in.readObject();
+         first = (ObjTypeFirst) in.readObject();
+         second = (ObjTypeSecond) in.readObject();
     }
 
     private void readObjectNoData() throws ObjectStreamException {
